0438ac25bc13e1bf3b159eca1a85654bca9f513a
Alisa Chavez <golimbievsky.a@gmail.com>
2023-09-04 12:21:52 -0500

    Added useRouter to the contact page

---
+++ src/pages/contact/index.js
@@ -0,0 +1,12 @@
+import { useRouter } from "next/router";
+
+const Contact = () => {
+    const router = useRouter();
+    
+    return (
+        <>
+        <h1>Contact</h1>
+        </>
+    );
+}
+export default Contact;
