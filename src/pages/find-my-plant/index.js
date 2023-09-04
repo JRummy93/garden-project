c310895d85cc30cc5abb7a325d9ab539cbaa61ef
Alisa Chavez <golimbievsky.a@gmail.com>
2023-09-04 12:18:47 -0500

    added useRouter to find-my-plant page

---
+++ src/pages/find-my-plant/index.js
@@ -0,0 +1,13 @@
+import { useRouter } from "next/router";
+
+const FindMyPlant = () => {
+    const router = useRouter();
+    
+    return (
+        <>
+        <h1>Find My Plant</h1>
+        </>
+    );
+}
+
+export default FindMyPlant;
