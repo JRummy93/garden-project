cdedc31b03c83e9a45ebd642290c65f3de5d2d21
Alisa Chavez <golimbievsky.a@gmail.com>
2023-09-06 17:36:31 -0500

    added theme to app.js and a Button on index.js to test it

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,30 @@
-import '@/styles/global.css'
+import "@/styles/global.css";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#4ed866",
+    },
+    secondary: {
+      main: "#baf2bb",
+    },
+    background: {
+      default: "#e8f2f1",
+    },
+    error: {
+      main: "#ff5722",
+    },
+    warning: {
+      main: "#a28622",
+    },
+  },
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
