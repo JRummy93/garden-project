f2154018a7425ecbfd882f1b1bc492fb2085fae5
Alisa Chavez <golimbievsky.a@gmail.com>
2023-09-08 09:37:32 -0500

    Begining process of Navbar. Copied it from MUI, and added MUI Icons to package

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,176 @@
-import React from 'react';
+import * as React from 'react';
+import {
+  Tooltip,
+  MenuItem,
+  Button,
+  Avatar,
+  Container,
+  Menu,
+  Typography,
+  IconButton,
+  Box,
+  AppBar,
+  Stack,
+  Toolbar
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useState } from "react";
+
+const pages = [
+  { label: "Explore", path: "/explore" },
+  { label: "My Garden", path: "/my-garden" },
+  { label: "Find My Plant", path: "/find-my-plant" },
+  { label: "Contact Us", path: "/contact" },
+  { label: "About Us", path: "/about" },
+];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 const Navbar = () => {
-  return <div>This is the navbar</div>;
-};
+  const [anchorElNav, setAnchorElNav] = useState(null);
+  const [anchorElUser, setAnchorElUser] = useState(null);
 
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        {" "}
+        {/* This is a Material UI component that sets the max width of the container to 100% of the viewport */}
+        <Toolbar disableGutters>
+         
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            LOGO
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            {" "}
+            {/* using display is similar to mediaquery */}
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map(({ label, path }) => (
+                <MenuItem key={label} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{label}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+         
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            LOGO
+          </Typography>
+          <Stack
+            direction="row"
+            display={{ xs: "none", md: "flex" }}
+            alignItems="center"
+            flexGrow={1} // this is saying take up all the space you can
+          >
+            {" "}
+            {/* this is essentially saying hide this on mobile, while the above was show on mobile this mobile menu */}
+            {pages.map(({ label, path }) => (
+              <NavbarItem label={label} path={path} />
+            ))}
+          </Stack>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
 export default Navbar;
