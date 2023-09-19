bbd83d947c698c65e9c88769e3c7589dff1f5b54
JRummy93 <jbmurrayokc@gmail.com>
2023-09-19 16:09:39 -0500

    Establish express proxy server for API CORS issue.

--- src/pages/api/explore/index.js
+++ src/pages/api/explore/index.js
@@ -1,56 +1,14 @@
-const axios = require('axios');
+import axios from 'axios';
 
-// async function makePerenualAPIRequest(pageCount, key, ) {
-//   try {
-//     const response = await axios.get('https://trefle.io/api/v1/plants', {
-//       params: {
-//         page: pageCount,
-//         token: 'u07N7SHuaI0uvGrVLkiI2zWl7PLb6ZgwGAfYE9SySnA'
-//       }
-//     });
-    
-//     return response.data;
-    
-//   } catch (error) {
-//     console.error('Error making API request:', error);
-//     throw error;
-//   }
-// }
-
-
-// export default makePerenualAPIRequest;
-
-
-async function makeTrefleAPIRequest() {
-  
-  const axios = require('axios');
-
-  const options = {
-    method: 'POST',
-    url: 'https://cors-proxy1.p.rapidapi.com/v1',
-    headers: {
-      'content-type': 'application/json',
-      'X-RapidAPI-Key': '568ab69ae0msh71c39f9affe3d57p180706jsnc5941a5cc2a7',
-      'X-RapidAPI-Host': 'cors-proxy1.p.rapidapi.com'
-    },
-    data: {
-      url: 'https://trefle.io/api/v1/plants',
-      method: 'GET',
-      params: {
-        token: 'u07N7SHuaI0uvGrVLkiI2zWl7PLb6ZgwGAfYE9SySnA'
-      },
-      data: {},
-      json_data: {},
-      headers: {},
-      cookies: {}
-    }
-  };
-  
+async function FetchTrefle() {
   try {
-    const response = await axios.request(options);
-    console.log(response.data);
+    const response = await fetch("http://localhost:3000");
+    const result = await response.json();
+    return result;
   } catch (error) {
-    console.error(error);
+    console.error("Error fetching data:", error);
+    throw error;
   }
 }
-export default makeTrefleAPIRequest;
+
+export default FetchTrefle;
--- src/pages/explore/index.js
+++ src/pages/explore/index.js
@@ -1,25 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import Head from 'next/head';
-import makeTrefleAPIRequest from '../api/explore';
 import PlantSearch from '@/components/PlantSearch';
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
+import FetchTrefle from '../api/explore/index';
 
-
-// export const getStaticProps = async () => {
-//   const result = await makePerenualAPIRequest();
-//   return { props: {result}}
-// }
-  export default function Explore() {
+export default function Explore() {
     const [result, setResult] = useState([]);
   
     useEffect(() => {
-      makeTrefleAPIRequest().then((responseResult) => {
+      FetchTrefle().then((responseResult) => {
         setResult(responseResult);
       });
     }, []);
-    const response = JSON.stringify(result.text);
-    console.log(response);
+    
+    
   
     return (
       <>
@@ -33,8 +28,8 @@ import Footer from "../../components/Footer";
         <Navbar />
           <h1>Explore</h1>
           <PlantSearch />
-          {Array.isArray(response) && response.length > 0 ? (
-            result.map((data) => (
+          {Array.isArray(result.data) && result.data.length > 0 ? (
+            result.data.map((data) => (
               <div key={data.id}>
                 <h2>{data.common_name}</h2>
                 <p>{data.scientific_name}</p>
