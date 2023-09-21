1debbdebafe142d3a35dfe67cfe8abf3916bb200
Alisa Chavez <golimbievsky.a@gmail.com>
2023-09-21 18:51:39 -0500

    deleted old user menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -260,33 +260,6 @@ const Navbar = () => {
           </Box>
           <Box sx={{ flexGrow: 0 }}>
             <UserButton />
-            {/* <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: "45px" }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu> */}
           </Box>
         </Toolbar>
       </Container>
