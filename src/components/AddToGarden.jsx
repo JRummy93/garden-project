ccd12dff2385b2b0895390f9c54ffbd86902af9d
Alisa Chavez <golimbievsky.a@gmail.com>
2023-09-27 16:22:24 -0500

    changed env file to new mongo link with password

--- src/components/AddToGarden.jsx
+++ src/components/AddToGarden.jsx
@@ -1,12 +1,43 @@
 import { useRouter } from "next/router";
 import { useAuth, SignUpButton } from "@clerk/nextjs";
-import Typography from "@mui/material/Typography";
-import { Button, CardActionArea, CardActions } from "@mui/material";
+import { Button } from "@mui/material";
+import { useState, useEffect } from "react";
 
-
-const AddToGarden = () => {
-    const router = useRouter();
+const AddToGarden = ({ PlantId }) => {
+  const router = useRouter();
   const { userId } = useAuth();
+  const [addedToGarden, setAddedToGarden] = useState(false);
+
+
+  const addToMyGarden = async () => {
+    // Check if the user is logged in
+    if (userId) {
+      // Send a request to your server to add the plant to the user's garden
+      try {
+        const response = await fetch("/api/add-to-garden", {
+          method: "POST",
+          body: JSON.stringify({ userId, plantId }), // Send the user and plant ID to your server
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        if (response.ok) {
+          // If the request was successful, update the UI to show "Added to My Garden"
+          setAddedToGarden(true);
+        } else {
+          // Handle errors if needed
+          console.error("Failed to add the plant to My Garden");
+        }
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    } else {
+      // If the user is not logged in, show the sign-up modal
+      // (You can customize this part according to your UI)
+      router.push("/sign-up"); // Redirect to the sign-up page
+    }
+  };
 
   return userId ? (
     <Button
--- src/components/PlantCard.jsx
+++ src/components/PlantCard.jsx
@@ -3,15 +3,12 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import { Button, CardActionArea, CardActions } from "@mui/material";
+import { CardActionArea, CardActions } from "@mui/material";
 import { useRouter } from "next/router";
-import { useAuth, SignUpButton } from "@clerk/nextjs";
 import AddToGarden from "./AddToGarden";
 
 const PlantCard = ({ common_name, scientific_name, image_url, id }) => {
   const router = useRouter();
-
-  const { userId } = useAuth();
 
   return (
     <Card sx={{ maxWidth: 500, maxHeight: 500 }} key={id}>
@@ -36,7 +33,9 @@ const PlantCard = ({ common_name, scientific_name, image_url, id }) => {
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <AddToGarden />
+        <AddToGarden 
+        PlantId={id}
+        />
       </CardActions>
     </Card>
   );
