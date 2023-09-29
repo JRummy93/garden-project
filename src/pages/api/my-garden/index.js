1518b8b006f1675c6d2384c88ab7299ad1f099f5
Alisa Chavez <golimbievsky.a@gmail.com>
2023-09-29 16:42:46 -0500

    adding a GET method

--- src/pages/api/my-garden/index.js
+++ src/pages/api/my-garden/index.js
@@ -1,32 +1,17 @@
 import plantSchema from "../../../server/Plants.js";
 import MyGardenSchema from "../../../server/MyGarden.js";
-import { getSession } from "@clerk/nextjs";
 import dbConnect from "@/server/db.js";
-
 const plantId = plantSchema.data.id;
+const Trefle_API_KEY = "token=u07N7SHuaI0uvGrVLkiI2zWl7PLb6ZgwGAfYE9SySnA";
+
 
 export default async function handler(req, res) {
-    const db = await dbConnect();
-    const userId = session.user.id;
+  await dbConnect();
 
-    if (req.method === 'GET') {
-          const session = await getSession({ req });
-    
-          if (!session) {
-            return res.status(401).json({ error: 'Unauthorized' });
-          }
-
-          const userGarden = .findOne({ UserId: userId });
-    
-          if (!userGarden) {
-            return res.status(404).json({ error: 'User garden not found' });
-          }
-    
-          return res.status(200).json(userGarden.MyPlants);
-        } catch (error) {
-          console.error('Error fetching user garden:', error);
-          return res.status(500).json({ error: 'Server error' });
-        }
-      }
-    
+  if (req.method === "GET") {
+    const response = await fetch(
+      `https://trefle.io/api/v1/plants?${Trefle_API_KEY}`
+    );
+  } else if (req.method === "POST") {
+  }
 }
