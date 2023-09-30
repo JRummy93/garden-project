4b44f559b7c2fcd4c20aa4db538f60fec0358bbe
Alisa Chavez <golimbievsky.a@gmail.com>
2023-09-30 11:25:40 -0500

    added a few extra plant details to page

--- src/pages/explore/[id].js
+++ src/pages/explore/[id].js
@@ -2,48 +2,67 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
-import { Container, Stack } from "@mui/material";
+import { Container, Stack, Typography, Box } from "@mui/material";
 
-  const PlantDetailPage = () => {
+const PlantDetailPage = () => {
   const [Plant, setPlant] = useState({});
   const router = useRouter();
 
   useEffect(() => {
-    
-  async function fetchPlant() {
-    
-    const id = router.query.id;
-    fetch(`/api/explore/${id}`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    }, 
-  })
-    .then(response => response.json())
-    .then(data => {
-      setPlant(data);
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
-};  
+    async function fetchPlant() {
+      const id = router.query.id;
+      fetch(`/api/explore/${id}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          setPlant(data);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    }
     fetchPlant();
-    }, [router.query.id]);
+  }, [router.query.id]);
 
   return (
     <>
       <Navbar />
       <Container>
         <Stack alignItems="center" spacing={4} p={4}>
-            <h1>Plant Details</h1>
-            {Plant.data && (
-              <>
-                 <h2>{Plant.data.common_name}</h2>
-                <h2>{Plant.data.id}</h2>
-                <h2>{Plant.data.scientific_name}</h2>
-                <img src={Plant.data.image_url} alt="default plant image" style={{ width: "50%" , height: "50%" }}></img>
-              </>
-            )}
+          <Typography variant="h2" color={"primary.dark"}>
+            Plant Details
+          </Typography>
+          <hr className="bar" />
+          {Plant.data && (
+            <>
+              <Typography variant="h3">{Plant.data.common_name}</Typography>
+              <Typography variant="h4" color={"#446652"}>
+                {Plant.data.scientific_name}
+              </Typography>
+              <Typography variant="p" color={"#446652"}>
+                ID#: {Plant.data.id}
+              </Typography>
+              <Stack direction={"row"}>
+                <img
+                  src={Plant.data.image_url}
+                  alt="default plant image"
+                  style={{ width: "50%", height: "50%" }}
+                ></img>
+                <div className="idDetailsDiv">
+                  <Typography variant="h5">
+                    <u>Family:</u> {Plant.data.main_species.family_common_name}
+                  </Typography>
+                  <Typography variant="h5">
+                    <u>Genus:</u> {Plant.data.main_species.genus}
+                  </Typography>
+                </div>
+              </Stack>
+            </>
+          )}
         </Stack>
       </Container>
       <Footer />
